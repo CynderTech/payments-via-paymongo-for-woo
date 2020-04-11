99dcc1fa9b76ddad9aa4da106341a7f5b4726209
Paul Peria <periabytes@gmail.com>
2020-04-12 03:23:54 +0800

    added logger

---
+++ classes/wc-paymongo-logger.php
@@ -0,0 +1,55 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * Log all things!
+ *
+ */
+class WC_Paymongo_Logger {
+
+	public static $logger;
+	const WC_LOG_FILENAME = 'woocommerce-gateway-paymongo';
+
+	/**
+	 * Utilize WC logger class
+	 *
+	 */
+	public static function log( $message, $start_time = null, $end_time = null ) {
+		if ( ! class_exists( 'WC_Logger' ) ) {
+			return;
+		}
+
+		if ( apply_filters( 'wc_paymongo_logging', true, $message ) ) {
+			if ( empty( self::$logger ) ) {
+				self::$logger = wc_get_logger();
+			}
+
+			$settings = get_option( 'woocommerce_paymongo_settings' );
+
+			if ( empty( $settings ) || isset( $settings['logging'] ) && 'yes' !== $settings['logging'] ) {
+				return;
+			}
+
+			if ( ! is_null( $start_time ) ) {
+
+				$formatted_start_time = date_i18n( get_option( 'date_format' ) . ' g:ia', $start_time );
+				$end_time             = is_null( $end_time ) ? current_time( 'timestamp' ) : $end_time;
+				$formatted_end_time   = date_i18n( get_option( 'date_format' ) . ' g:ia', $end_time );
+				$elapsed_time         = round( abs( $end_time - $start_time ) / 60, 2 );
+
+				$log_entry  = "\n" . '====Paymongo Version: ' . WC_PAYMONGO_VERSION . '====' . "\n";
+				$log_entry .= '====Start Log ' . $formatted_start_time . '====' . "\n" . $message . "\n";
+				$log_entry .= '====End Log ' . $formatted_end_time . ' (' . $elapsed_time . ')====' . "\n\n";
+
+			} else {
+				$log_entry  = "\n" . '====Paymongo Version: ' . WC_PAYMONGO_VERSION . '====' . "\n";
+				$log_entry .= '====Start Log====' . "\n" . $message . "\n" . '====End Log====' . "\n\n";
+
+			}
+
+			self::$logger->debug( $log_entry, array( 'source' => self::WC_LOG_FILENAME ) );
+		}
+	}
+}
