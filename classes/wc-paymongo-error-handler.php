a57819a41f3608f079879876efd7fdee904ce51d
Paul Peria <periabytes@gmail.com>
2020-04-14 15:02:38 +0800

    add error handler

---
+++ classes/wc-paymongo-error-handler.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * PHP version 7
+ * 
+ * Helper for PayMongo errors
+ * 
+ * @category Plugin
+ * @package  PayMongo
+ * @author   PayMongo <developers@paymongo.com>
+ * @license  n/a (http://127.0.0.0)
+ * @link     n/a
+ */
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+/**
+ * Parse PayMongo Errors
+ * 
+ * @category Class
+ * @package  PayMongo
+ * @author   PayMongo <developers@paymongo.com>
+ * @license  n/a (http://127.0.0.0)
+ * @link     n/a
+ */
+class WC_PayMongo_Error_Handler
+{
+    /**
+     * Build Error messages from PayMongo errors
+     * 
+     * @param array $errors PayMongo Errors body
+     * 
+     * @return string
+     */
+    public static function parseErrors($errors)
+    {
+        if (!isset($errors) || count($errors) === $errors) {
+            return '<div class="woocommerce-error">'
+                . 'Something went wrong. Unable to retrieve error record'
+                . '</div>';
+        }
+
+        $messages = '<ul class="woocommerce-error">';
+
+        foreach ($errors as $error) {
+            $messages .= '<li>' .
+                $error['detail'] .
+                ' (code: ' . $error['code'] . ')' .
+                '</li>';
+        }
+
+        $messages .= '</ul>';
+
+        return $messages;
+    }
+}
